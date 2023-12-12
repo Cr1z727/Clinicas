6bd76ee2fdec6def49eac0b81df8563e2b631d26
Cr1z727 <christiancamarg2016@gmail.com>
2023-12-11 21:10:50 -0500

    Actualizando repositorio

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const clinicas = require("./modelos")
-
+const clinicas = require("./modelos");
+const {swaggerDocs} = require('./swagger');
 const app = express();
 const port = 3000;
 app.use(express.json())
 
+/**
+ * @swagger
+ * tags:
+ *   name: Conección
+ *   description: Operaciones relacionadas con la conexión a la base de datos.
+ */
 async function coneccion (){await mongoose.connect('mongodb+srv://CristianCamargo:Cristian227700@cluster0.ssvjvjq.mongodb.net/tablas?retryWrites=true&w=majority');console.log("esta conectado")}
-
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     ApiKeyAuth:
+ *       type: apiKey
+ *       in: header
+ *       name: x-api-key
+ * 
+ * security:
+ *   - ApiKeyAuth: []
+ */
 require('dotenv').config()
 
 const API_KEY = process.env.API_KEY
@@ -24,6 +41,33 @@ const apiKeyValidation = (req,res,next) =>{
 
 app.use(apiKeyValidation)
 
+/**
+ * @swagger
+ * /patients/{id}:
+ *   get:
+ *     summary: Obtiene información de un paciente por ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del paciente.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Información del paciente obtenida con éxito.
+ *         content:
+ *           application/json:
+ *             example:
+ *               _id: "5f4e8bb97e3e414772e30106"
+ *               name: "Nombre del Paciente"
+ *               // Otros campos del paciente
+ *       404:
+ *         description: No se encontró ningún paciente con el ID proporcionado.
+ *       500:
+ *         description: Error interno del servidor.
+ */
+
 app.get("/patients/:id",async (req, res)=>{ 
   
   coneccion()
@@ -31,9 +75,30 @@ app.get("/patients/:id",async (req, res)=>{
   console.log(usuario)
   res.status(200).send(usuario)
   return JSON.parse(JSON.stringify(usuario))
-  //res.json(usuario);
+  
 });
 
+/**
+ * @swagger
+ * /patients:
+ *   get:
+ *     summary: Obtiene la lista de todos los pacientes.
+ *     responses:
+ *       200:
+ *         description: Lista de pacientes obtenida con éxito.
+ *         content:
+ *           application/json:
+ *             example:
+ *               - _id: "5f4e8bb97e3e414772e30106"
+ *                 name: "Nombre del Paciente 1"
+ *                 // Otros campos del paciente 1
+ *               - _id: "5f4e8bb97e3e414772e30107"
+ *                 name: "Nombre del Paciente 2"
+ *                 // Otros campos del paciente 2
+ *               // Otros pacientes en la lista
+ *       500:
+ *         description: Error interno del servidor.
+ */
 app.get("/patients",async (req, res)=>{ 
   
         coneccion()
@@ -41,10 +106,33 @@ app.get("/patients",async (req, res)=>{
         console.log(registros)
         res.status(200).send(registros)
         return JSON.parse(JSON.stringify(registros))
-        //res.json(registros);
+       
 });
 
-
+/**
+ * @swagger
+ * /patients:
+ *   post:
+ *     summary: Agrega un nuevo paciente a la base de datos.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             - name: "Nuevo Paciente"
+ *               // Otros campos del nuevo paciente
+ *     responses:
+ *       201:
+ *         description: Paciente agregado con éxito.
+ *         content:
+ *           application/json:
+ *             example:
+ *               _id: "5f4e8bb97e3e414772e30108"
+ *               name: "Nuevo Paciente"
+ *               // Otros campos del nuevo paciente
+ *       500:
+ *         description: Error interno del servidor.
+ */
 
 app.post('/patients', async (req, res) => {
   coneccion()
@@ -57,6 +145,45 @@ app.post('/patients', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /patients/put/{id}:
+ *   put:
+ *     summary: Actualiza la información de un paciente por ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del paciente a actualizar.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             nombre: "Nuevo Nombre"
+ *             motivo: "Nuevo Motivo"
+ *             doctor: "Nuevo Doctor"
+ *             notas: "Nuevas Notas"
+ *             foto: "Nueva Foto"
+ *     responses:
+ *       200:
+ *         description: Información del paciente actualizada con éxito.
+ *         content:
+ *           application/json:
+ *             example:
+ *               _id: "5f4e8bb97e3e414772e30108"
+ *               nombre: "Nuevo Nombre"
+ *               motivo: "Nuevo Motivo"
+ *               doctor: "Nuevo Doctor"
+ *               notas: "Nuevas Notas"
+ *               foto: "Nueva Foto"
+ *       404:
+ *         description: No se encontró ningún paciente con el ID proporcionado.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 app.put('/patients/put/:id', async (req, res) => {
 
   const { id } = req.params;
@@ -79,7 +206,26 @@ app.put('/patients/put/:id', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
+/**
+ * @swagger
+ * /patients/delete/{id}:
+ *   delete:
+ *     summary: Elimina un paciente por ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del paciente a eliminar.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Paciente eliminado exitosamente.
+ *       404:
+ *         description: No se encontró ningún paciente con el ID proporcionado.
+ *       500:
+ *         description: Error interno del servidor al intentar eliminar el paciente.
+ */
 app.delete("/patients/delete/:id",async(req,res)=>{
   coneccion()
   
@@ -100,7 +246,7 @@ app.delete("/patients/delete/:id",async(req,res)=>{
   
   
   return JSON.parse(JSON.stringify(findPacientes))
-  //res.status(200).json({success: true, data : findPacientes})
+  
 })
 
 
@@ -113,6 +259,7 @@ app.delete("/patients/delete/:id",async(req,res)=>{
 
 app.listen(port, ()=> {
     console.log("the app is running");
+    swaggerDocs(app, port);
 })
 
 
---
+++ swagger.js
@@ -0,0 +1,28 @@
+const swaggerJSDOC = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// Metadata informacion. Pilas, el versionamiento es por que es comun tener 
+// diferentes versiones de nuestra API
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info:{
+            title: "Api para historia clinica",
+            version: "1.0.0",
+        }
+    },
+    apis:[
+        './app.js'
+    ]
+}
+
+// Documentacion en formato Json
+// Obviamente json!!!
+const swaggerSpec = swaggerJSDOC(options);
+
+const swaggerDocs = (app,port)=>{
+    app.use('/doc-ap-v1',swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+    
+}
+
+module.exports = {swaggerDocs}
